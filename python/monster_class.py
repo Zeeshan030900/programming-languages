3fd9faf98b9f646e4a2d8f4c09e93cbd62e52715
zeeshan khan <zeeshankhn123@outlook.com>
2023-11-25 20:47:33 +0000

    Update classes and methods in monster_class.py and player_class.py
    - Monster class has been updated with additional comments explaining its attributes and methods.
    - The attack method in Monster class has been updated to print a message about the attack.
    - Player class has been updated with additional comments explaining its attributes and methods.
    - The attack method in Player class has been updated to include a check for attackDelay before attacking.
    - The choose_weapon method in Player class now sets the attackDelay according to the chosen weapon.
    
    Refactor classes and methods in monster_class.py and player_class.py

--- python/monster_class.py
+++ python/monster_class.py
@@ -1,11 +1,23 @@
 from living_being_class import LivingBeing
 
 
+# Class Monster
+# Inherits from the 'LivingBeing' class. It represents the monsters within the game.
+# Monsters have the same attributes as any living being, such as name and health, but also have specific attack damage,
+# which will be used to hurt other players.
 class Monster(LivingBeing):
+
+    # Attributes:
+    # - name (str): Represents the name of the monster.
+    # - health (int or float): Represents the health points of the monster. Decreases when the monster gets attacked.
+    # - attack_damage (int or float): Represents the amount of damage the monster can inflict on the player.
     def __init__(self, name, health, attack_damage):
         super().__init__(name, health)
         self.attackdamage = attack_damage
 
+    # Methods:
+    # - attack(self, player): Inflicts damage to the player equal to the monster's attack damage.
+    # This also prints a message about the attack.
     def attack(self, player):
         print(f"{self.name} attacked {player.name}. Attack did {self.attackdamage} amount of damage")
         player.health -= self.attackdamage
--- python/player_class.py
+++ python/player_class.py
@@ -1,12 +1,24 @@
 from living_being_class import LivingBeing
 
 
+# Class Player
+# Inherits from the LivingBeing class.
+# Represents a player in the game with certain functionalities such as attacking and choosing weapons.
 class Player(LivingBeing):
+
+    # Attributes:
+    # - name (str): The name of the Player
+    # - health (int): The Player's health points. Inherited from LivingBeing class.
+    # - attackDelay (int): The delay for the Player's attack. Equal to 0 by default.
+    # - weapon (object): The object representing the weapon of the Player.
+
     def __init__(self, name, health):
         super().__init__(name, health)
         self.attackDelay = 0
         self.weapon = None
 
+    # Method "attack" is used for the Player to attack a Monster.
+    # If attackDelay is 0, the Player can attack; otherwise, it has to wait the required number of turns.
     def attack(self, monster):
         if self.attackDelay == 0:
             print(f"{self.name} attacked {monster.name} with a {self.weapon.name}",
@@ -18,6 +30,8 @@ class Player(LivingBeing):
             print(f"{self.name} has {self.attackDelay} turns to wait before attacking")
             self.attackDelay -= 1
 
+    # Method "choose_weapon" allows the Player to choose a weapon from the given list of weapons.
+    # It also sets the attackDelay according to the chosen weapon.
     def choose_weapon(self, weapons):
         print("Choose a weapon from this list:")
         for index, weapon in weapons.items():
